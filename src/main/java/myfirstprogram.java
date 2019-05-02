b44516a7cc2fe09c3333ede5dc825152434223e1
Pavel Tkalin <tkalin-pavel@yandex.ru>
2019-05-02 17:03:10 +0300

    function learning

--- src/main/java/myfirstprogram.java
+++ src/main/java/myfirstprogram.java
@@ -5,10 +5,44 @@ public class myfirstprogram {
     public static void main(String []args)
 
     {
-        System.out.println("Good luck");
+
+
+        Hello(" " + "John");
+        Hello(" " +"Ivan");
+        
+
+        double l = 5;
+
+        System.out.println("Square is " + area(l));
+
+        double barb = 5.6;
+
+        System.out.println("Square rectangle=  "+area(barb,2));
+
+
+
 
     }
 
+    public static void Hello(String somebody)
+
+    {
+
+        System.out.println("Hi"+somebody);
+    }
 
 
+    public static double area (double len)
+
+    {
+
+        return len*len;
+    }
+
+    public static double area (double len, double b)
+
+    {
+
+        return len*b;
+    }
 }
